7a16d31de2c6d435ca07cc5dbcbb9bbb19f0aadc
Matt Sharkey <msharkey3434@gmail.com>
2019-08-02 12:40:50 -0500

    Finish write up

--- 01-SQL_SAT_Presentation/visualizeLoadTest.R
+++ 01-SQL_SAT_Presentation/visualizeLoadTest.R
@@ -3,7 +3,7 @@ df <- shinyloadtest::load_runs(
   "50workersCache" = "./test-logs-2019-07-22T11_36_05.908Z" ,
   "50workers" = "./test-logs-2019-07-22T03_21_19.997Z")
 
-shinyloadtest::shinyloadtest_report(df, "run3.html")
+shinyloadtest::shinyloadtest_report(df, "run4.html")
 
 
 df <- shinyloadtest::load_runs("50workers" = "./test-logs-2019-07-22T03_21_19.997Z")
